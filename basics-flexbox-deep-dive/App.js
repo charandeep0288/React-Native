d14f9d989eca30c8fd455967f4b2e7a9e3b0a5a1
charandeep0288 <ckaunsel123@gmail.com>
2023-02-05 22:29:02 +0530

    Basics-flexbox-deep-dive: Completed concept of Flex Box

--- basics-flexbox-deep-dive/App.js
+++ basics-flexbox-deep-dive/App.js
@@ -1,38 +1,54 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
+    // By defalut: Every View in the React Native uses Flex Box, but in web it is not like that
+    // By defalut: Every View in the React Native uses Flex Direction of column
+    // {/* flexDirection: row-reverse */}
+    // For flexDirection: 'row', "justifyContent" is used to align items on the "Main axis" & "alignItems" is used to align items on the "Cross axis"
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "80%",
+        height: 300,
+        justifyContent: "space-around",
+        alignItems: "stretch",
+      }}
+    >
       <View
         style={{
-          backgroundColor: 'red',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
+          backgroundColor: "red",
+          // width: 100, 
+          // height: 100,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Text>1</Text>
       </View>
       <View
         style={{
-          backgroundColor: 'blue',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
+          backgroundColor: "blue",
+          // width: 100,
+          // height: 100,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Text>2</Text>
       </View>
       <View
         style={{
-          backgroundColor: 'green',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
+          backgroundColor: "green",
+          // width: 100,
+          // height: 100,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Text>3</Text>
