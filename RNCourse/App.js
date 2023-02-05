71d8751c9584dd04f53fcf7af14d6f0f4d94e1df
charandeep0288 <ckaunsel123@gmail.com>
2023-02-05 23:28:45 +0530

    22. & 23. Get value from TextInput, collect them in Array onPress of ADD GOAL btn and DISPAY VALUES IN LIST OF GOALS using map fn

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,17 +1,38 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native"; // We have to import each thing we want to use, un like in React JS where we were able to use the HTML btn without importing it or anything.
 
 // {Core Components} -> View, Text, Image, ScrollView, TextInput
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState(""); // Intial State => "" Empty String
+  const [courseGoals, setCourseGoals] = useState([]); // Intial State => [] Empty Array
+
+  function goalInputHandler(enteredText) {
+    // console.log(enteredText);
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    // console.log(enteredGoalText);
+    // setCourseGoals([...courseGoals, enteredGoalText]); // NOT BEST WAY OF UPDATING STATE because new state is dependent on old state.
+    setCourseGoals((currentCourseGoals) => [...courseGoals, enteredGoalText]); // BETTER OR RECOMMENDED WAY TO UPDATING STATE, "currentCourseGoals" would be provided by React
+  }
+
   return (
     <View style={styles.appContainer}>
       {/* 1st View have input area */}
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       {/* 2nd View is used to view the list of goals that we have entered*/}
-      <View>
-        <Text>List of Goals......</Text>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => {
+          return <Text key={goal}>{goal}</Text>; // we can do this because goal is just a string
+        })}
       </View>
     </View>
   );
@@ -20,19 +41,30 @@ export default function App() {
 // It is prefered to use this "StyleSheet" to seperate JSX and styling code. And make CSS reuseable.
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
 
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#cccccc",
   },
 
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
+    borderColor: "#cccccc",
+    width: "70%",
     marginRight: 8,
-    padding: 8
-  }
+    padding: 8,
+  },
+
+  goalsContainer: {
+    flex: 4,
+  },
 });
