17656b9fcbf57a07bf1746ea89820537764b9d49
charandeep0288 <ckaunsel123@gmail.com>
2023-02-05 18:40:58 +0530

    React Native: applied Flexbox on RNCourse app

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,53 +1,38 @@
-import { StyleSheet, Text, View, Button } from "react-native"; // We have to import each thing we want to use, un like in React JS where we were able to use the HTML btn without importing it or anything.
+import { StyleSheet, Text, View, Button, TextInput } from "react-native"; // We have to import each thing we want to use, un like in React JS where we were able to use the HTML btn without importing it or anything.
 
 // {Core Components} -> View, Text, Image, ScrollView, TextInput
 export default function App() {
   return (
-    // <View> eqivaltent to <div>, <scction>, <article> in HTML, <View> is used to hold other things(components) not text, <View> is also used to structure or layout other components.
-    <View style={styles.container}>
-      <View>
-        <Text
-          // style={{
-          //   margin: 16,
-          //   borderWidth: 2,
-          //   borderColor: "red",
-          //   padding: 16,
-          // }}
-          style={styles.dummyText}
-        >
-          Another piece of text!
-        </Text>
+    <View style={styles.appContainer}>
+      {/* 1st View have input area */}
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
+        <Button title="Add Goal" />
       </View>
-      {/* {Inline styling} using style attribute */}
-      <Text
-        style={styles.dummyText}
-        // style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}
-      >
-        Hello World
-      </Text>
-      {/* this is self closing tag  <Button> */}
-      <Button title="tap me!" />
+      {/* 2nd View is used to view the list of goals that we have entered*/}
+      <View>
+        <Text>List of Goals......</Text>
+      </View>
     </View>
   );
 }
 
-
-// const styles2 = {}; //  we could have created a new styled component like this and set styles here.
-// but "StyleSheet" have a advantage of checking for valid style properties or values, and also give us auto-completion functionality.
-
 // It is prefered to use this "StyleSheet" to seperate JSX and styling code. And make CSS reuseable.
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  dummyText: {
-    // value should be object with CSS we want it to hold on.
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "blue",
-    padding: 16,
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+  }
 });
