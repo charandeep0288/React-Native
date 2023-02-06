29f3d3436f69547468c09f141e510d543794db9d
charandeep0288 <ckaunsel123@gmail.com>
2023-02-06 23:33:47 +0530

    47. Setting Up our Screen Components - TextInput & 2 btns on StartGameScreen

---
+++ Target_App/screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { View, TextInput } from "react-native";
+
+function StartGameScreen() {
+    return (
+        <View>
+            <TextInput />
+            {/* BUTTONS - going to create custom button using <View> */}
+            
+        </View>
+    );
+}
+
+export default StartGameScreen;
