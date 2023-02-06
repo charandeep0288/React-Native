5e8bf825e6a4fefe46e820debdd9a73d52141f37
charandeep0288 <ckaunsel123@gmail.com>
2023-02-06 12:28:12 +0530

    32. Adding an Android Ripple Effect & an iOS Alternative

--- RNCourse/components/GoalItem.js
+++ RNCourse/components/GoalItem.js
@@ -3,26 +3,36 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 // Touchable(all realted to this, these are old not), Pressable(Touchable and all related to this are replaced by Pressable) are components through which we tell react-native that this component is pressable.
 function GoalItem(props) {
-
   return (
     // .bind() is a standard JS fn which allows us to preconfigure a fn for future execution. First argument is "this" keyword in this bind() fn, 2nd argument would be first parameter recivied by to be called fn(in this case props.id that we want to remove )
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210635" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        // for iOS we have to do this to add a effect onPress of Item when we want to remove it.
+        // "style" can also take fn, this fn would be called automatically by <Pressable> whenever the Press state changes
+        // style={(pressedData) => pressedData.pressed}
+        style={({pressed}) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
 
+  pressedItem: {
+    opacity: 0.5
+  },
+  
   goalText: {
+    padding: 8,
     color: "white",
   },
 });
