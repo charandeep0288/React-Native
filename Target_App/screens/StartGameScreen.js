2253b7f8ffffac6cbd0d5bbd46532748dd690db7
charandeep0288 <ckaunsel123@gmail.com>
2023-02-07 12:50:24 +0530

    51. Configuring the TextInput Field - now we get numaric keypad

--- Target_App/screens/StartGameScreen.js
+++ Target_App/screens/StartGameScreen.js
@@ -2,42 +2,48 @@ import { StyleSheet, View, TextInput } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    return (
-        <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2}/>
-            {/* BUTTONS - going to create custom button using <View> */}
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
-        </View>
-    );
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false} // we don't want autoCorrect or autoCapitalize functionality here.
+      />
+      {/* BUTTONS - going to create custom button using <View> */}
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        marginTop: 100,
-        marginHorizontal: 24, 
-        padding: 16,
-        backgroundColor: "#76063c",
-        borderRadius: 8,
-        elevation: 4, // to add box shadow in android only
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#76063c",
+    borderRadius: 8,
+    elevation: 4, // to add box shadow in android only
 
-        // to add box shadow in iOS, we have to do these things 
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-    },
+    // to add box shadow in iOS, we have to do these things
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
 
-    numberInput: {
-        height: 50,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor: "#ddb52f",
-        borderBottomWidth: 2,
-        color: "#ddb52f",
-        marginVertical: 8,
-        bold: 8,
-        textAlign: "center",
-    }
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    bold: 8,
+    textAlign: "center",
+  },
 });
 export default StartGameScreen;
