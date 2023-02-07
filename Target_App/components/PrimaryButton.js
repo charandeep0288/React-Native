e7dbf1c7851b719b6608dcb193b572f774faff16
charandeep0288 <ckaunsel123@gmail.com>
2023-02-07 11:56:17 +0530

    48. Creating Custom Buttons - PrimaryButton.js & 'Reset' & 'Confirm' btn

---
+++ Target_App/components/PrimaryButton.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function PrimaryButton({children}) {
+    return (
+        <View>
+            <Text>{children}</Text>
+        </View>
+    );
+}
+
+export default PrimaryButton;
+
+
+const styles = StyleSheet.create({
+
+});
--- Target_App/screens/StartGameScreen.js
+++ Target_App/screens/StartGameScreen.js
@@ -1,11 +1,13 @@
 import { View, TextInput } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return (
         <View>
             <TextInput />
             {/* BUTTONS - going to create custom button using <View> */}
-            
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
         </View>
     );
 }
