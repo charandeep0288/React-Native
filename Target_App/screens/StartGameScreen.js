2074d0b10862288c5f1645b1d5f328d3e7312142
charandeep0288 <ckaunsel123@gmail.com>
2023-02-07 12:26:28 +0530

    49. Styling for Andriod & iOS - Applied shadow for Andriod & iOS both

--- Target_App/screens/StartGameScreen.js
+++ Target_App/screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { View, TextInput } from "react-native";
+import { StyleSheet, View, TextInput } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
             {/* BUTTONS - going to create custom button using <View> */}
             <PrimaryButton>Reset</PrimaryButton>
@@ -12,4 +12,20 @@ function StartGameScreen() {
     );
 }
 
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24, 
+        padding: 16,
+        backgroundColor: "#76063c",
+        borderRadius: 8,
+        elevation: 4, // to add box shadow in android only
+
+        // to add box shadow in iOS, we have to do these things 
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+    } 
+});
 export default StartGameScreen;
