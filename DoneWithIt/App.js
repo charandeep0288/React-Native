9378c3fb5a5517b67ee62fd8738020d18cba9b1e
charandeep0288 <ckaunsel123@gmail.com>
2023-02-08 15:33:28 +0530

    Youtube - DoneWithIt App - justifyContent, alignItems & alignSelf

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -2,10 +2,25 @@ import { StyleSheet, StatusBar, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={[{ backgroundColor: "green", flexDirection: 'row' }, styles.container]}>
+    <View
+      style={[
+        {
+          backgroundColor: "green",
+          flexDirection: "row", // horizontal - main axis
+          justifyContent: "center", // main (flex-start(default), flex-end, center), space-around, space-evenly, space-between
+          alignItems: "center", // cross axis (center, baseline, flex-end, flex-start, stretch(default))
+        },
+        styles.container,
+      ]}
+    >
       {/* by default items are layed out in a column flexDirection */}
-      <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 100, 
+        height: 300, 
+        alignSelf: "flex-start"
+       }}
+      />
+      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
@@ -15,9 +30,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-
 });
 // App URL
 // exp://exp.host/@charandeep2000/DoneWithIt?release-channel=default
