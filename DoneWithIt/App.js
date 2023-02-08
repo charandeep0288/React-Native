ff5ea9c27c6bce4a2b4067868af3e054d44c9899
charandeep0288 <ckaunsel123@gmail.com>
2023-02-08 15:47:01 +0530

    Youtube - DoneWithIt App - flexWrap & alignContent IMP

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -8,7 +8,10 @@ export default function App() {
           backgroundColor: "green",
           flexDirection: "row", // horizontal - main axis
           justifyContent: "center", // main (flex-start(default), flex-end, center), space-around, space-evenly, space-between
-          alignItems: "center", // cross axis (center, baseline, flex-end, flex-start, stretch(default))
+          alignItems: "center", // alignItems align items in each row(line) when we have flexWrap: "wrap" on cross axis
+          // cross axis (center, baseline, flex-end, flex-start, stretch(default))
+          alignContent: "center", // using this with flexWrap: "wrap", Items would be aligned in the center of the screen. "alignContent" only works with "flexWrap".
+          flexWrap: "wrap", // (no-wrap(default), )
         },
         styles.container,
       ]}
@@ -17,11 +20,14 @@ export default function App() {
       <View
         style={{ backgroundColor: "dodgerblue", width: 100, 
         height: 300, 
-        alignSelf: "flex-start"
+        // alignSelf: "flex-start"
        }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "grey", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "greenyellow", width: 100, height: 100 }} />
+      {/* by default: If ower Items overflow accross the main axis, then one or more items get shurk so that other Items can fit on the screen, can change this by enabling Wraping */}
     </View>
   );
 }
