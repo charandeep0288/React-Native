42e49de0d476798c62decdf4333e12eacda877a5
charandeep0288 <ckaunsel123@gmail.com>
2023-02-08 15:09:48 +0530

    Youtube - DoneWithIt App - FlexDirection(row, column, row-reverse, column-reverse)

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,14 +1,23 @@
-import { View } from "react-native";
+import { StyleSheet, StatusBar, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "1", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
-      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    <View style={[{ backgroundColor: "green", flexDirection: 'row' }, styles.container]}>
+      {/* by default items are layed out in a column flexDirection */}
+      <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+
+});
 // App URL
 // exp://exp.host/@charandeep2000/DoneWithIt?release-channel=default
