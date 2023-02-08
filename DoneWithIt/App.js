2a360d9f560cc8f7cd1088f3c1a68311dc32dfc7
charandeep0288 <ckaunsel123@gmail.com>
2023-02-08 16:56:14 +0530

    Youtube - DoneWithIt App - flexBasis, flexGrow, flexShrink

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -10,23 +10,27 @@ export default function App() {
           justifyContent: "center", // main (flex-start(default), flex-end, center), space-around, space-evenly, space-between
           alignItems: "center", // alignItems align items in each row(line) when we have flexWrap: "wrap" on cross axis
           // cross axis (center, baseline, flex-end, flex-start, stretch(default))
-          alignContent: "center", // using this with flexWrap: "wrap", Items would be aligned in the center of the screen. "alignContent" only works with "flexWrap".
-          flexWrap: "wrap", // (no-wrap(default), )
+          // alignContent: "center", // using this with flexWrap: "wrap", Items would be aligned in the center of the screen. "alignContent" only works with "flexWrap".
+          // flexWrap: "wrap", // (no-wrap(default), )
         },
         styles.container,
       ]}
     >
       {/* by default items are layed out in a column flexDirection */}
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, 
-        height: 300, 
-        // alignSelf: "flex-start"
+        style={{ backgroundColor: "dodgerblue", 
+        width: 400, 
+        height: 100, 
+        // flexBasis: 100, // Can be mapped to "width" OR "height", for this case -> It is same as width: 100
+        // flexGrow: 1, // same as flex: 1
+        flex: -1, // flex is a shot hand of flexGlow & flexShrink
+        // flexShrink: 1,
        }}
       />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "grey", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "greenyellow", width: 100, height: 100 }} />
+      {/* <View style={{ backgroundColor: "grey", width: 100, height: 100 }} /> */}
+      {/* <View style={{ backgroundColor: "greenyellow", width: 100, height: 100 }} /> */}
       {/* by default: If ower Items overflow accross the main axis, then one or more items get shurk so that other Items can fit on the screen, can change this by enabling Wraping */}
     </View>
   );
@@ -39,5 +43,6 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
+
 // App URL
 // exp://exp.host/@charandeep2000/DoneWithIt?release-channel=default
