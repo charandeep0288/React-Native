e1d8af8afbb151dbf25d18e9b43fecefac6ebec4
charandeep0288 <ckaunsel123@gmail.com>
2023-02-08 19:55:54 +0530

    54. Coloring the Components & The Overall App

--- Target_App/App.js
+++ Target_App/App.js
@@ -1,18 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    // By default -> <View> take as much space as they need to fit their content inside themself's.
+    <View style={styles.rootScreen}> 
+      <StartGameScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  rootScreen: {
+    flex: 1, // by adding flex: 1 here, this container would take as much space as available
+    backgroundColor: "#ddb52f", // next we would move to gradiant, then picture + gradiant
   },
 });
