c7e3f045ed8d0ca38ce5eb82800d864c23cd19d1
charandeep0288 <ckaunsel123@gmail.com>
2023-02-08 13:47:52 +0530

    Youtube - DoneWithIt App - Explored Dimensions

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,9 +1,20 @@
-import { SafeAreaView, StyleSheet, Button, Alert, StatusBar, Platform } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Button,
+  Alert,
+  StatusBar,
+  Platform,
+  View,
+  Dimensions
+} from "react-native";
 
 // View -> UIView
 export default function App() {
   let x = 1;
 
+  console.log(Dimensions.get("screen"));
+  
   // console.log("App executed");
   // console.log(require("./assets/icon.png")); // returns a number that is refernce to ower <Image>
   const handlePress = () => console.log("Text Clicked!");
@@ -12,24 +23,14 @@ export default function App() {
     // Styles coming from the left most(or last place) array element override the style which might came from the previous style components
     <SafeAreaView style={[styles.container, containerStyle]}>
       {/* SafeAreaView only works in iOS */}
-      <StatusBar style="auto" />
-      <Button
-        title="Click ME"
-        // onPress={() => Alert.alert("My title", "My message", [
-        //   {text: "Yes", onPress: () => console.log("Yes")},
-        //   {text: "No",  onPress: () => console.log("No")},
-        // ])}
-
-        onPress={
-          () =>
-            Alert.prompt("My title", "My message", (text) => console.log(text)) // 3rd parameter is callback fn, Only works on iOS for now
-        }
-      ></Button>
+      <View
+        style={{ backgroundColor: "dodgerblue", width: "50%", height: 70 }}
+      ></View>
     </SafeAreaView>
   );
 }
 
-const containerStyle = { backgroundColor: "orange"};
+const containerStyle = { backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
@@ -37,7 +38,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     // alignItems: "center",
     // justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currnentHeight : 0
+    paddingTop: Platform.OS === "android" ? StatusBar.currnentHeight : 0,
   },
 
   textImage: {
