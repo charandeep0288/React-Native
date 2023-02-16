8a27f361217237743b4502c50a95c17c4e95cee1
charandeep0288 <ckaunsel123@gmail.com>
2023-02-16 22:40:27 +0530

    72. Using & Styling Nested Text - did work on GameOverScreen.js

--- Target_App/screens/GameOverScreen.js
+++ Target_App/screens/GameOverScreen.js
@@ -1,5 +1,7 @@
 import { View, Image, StyleSheet, Text } from "react-native";
+
 import Title from "../components/ui/Title";
+import PrimaryButton from "../components/ui/PrimaryButton";
 import Colors from "../constants/colors";
 
 function GameOverScreen() {
@@ -10,9 +12,19 @@ function GameOverScreen() {
         <Image
           style={styles.image}
           source={require("../assets/images/success.png")}
-        ></Image>
+        />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y.</Text>
+      {/* A <Text> component can have another <Text> component but <Text> component cannot a <View> component */}
+      {/* <Text> component in a <Text> component, inner or nested <Text> component recieve the styles we have defined in the outer <Text> component */}
+      {/*And when we have <Text> component in a <View> component, inner <Text> component doesn't recieve the styles we have defined in the outer <View> component */}
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number
+        <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <View>
+        <PrimaryButton>Start New Game</PrimaryButton>
+      </View>
     </View>
   );
 }
@@ -39,5 +51,17 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
 });
