f7d1e81c104629cfbaa1411e134d415486e18902
charandeep0288 <ckaunsel123@gmail.com>
2023-02-17 17:53:05 +0530

    87. Further Improvments with useWindowDimensions - on GameOverScreen

--- Target_App/screens/GameOverScreen.js
+++ Target_App/screens/GameOverScreen.js
@@ -1,41 +1,72 @@
-import { View, Image, StyleSheet, Text, Dimensions } from "react-native";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Dimensions,
+  useWindowDimensions,
+} from "react-native";
 
 import Title from "../components/ui/Title";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Colors from "../constants/colors";
 
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 400) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  };
   return (
-    <View style={styles.rootContainer}>
-      <Title>GAME OVER!</Title>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require("../assets/images/success.png")}
-        />
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>GAME OVER!</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image
+            style={styles.image}
+            source={require("../assets/images/success.png")}
+          />
+        </View>
+        {/* A <Text> component can have another <Text> component but <Text> component cannot a <View> component */}
+        {/* <Text> component in a <Text> component, inner or nested <Text> component recieve the styles we have defined in the outer <Text> component */}
+        {/*And when we have <Text> component in a <View> component, inner <Text> component doesn't recieve the styles we have defined in the outer <View> component */}
+        <Text style={styles.summaryText}>
+          Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
+          rounds to guess the number
+          <Text style={styles.highlight}> {userNumber}</Text>.
+        </Text>
+        <View>
+          <PrimaryButton onPressProp={onStartNewGame}>
+            Start New Game
+          </PrimaryButton>
+        </View>
       </View>
-      {/* A <Text> component can have another <Text> component but <Text> component cannot a <View> component */}
-      {/* <Text> component in a <Text> component, inner or nested <Text> component recieve the styles we have defined in the outer <Text> component */}
-      {/*And when we have <Text> component in a <View> component, inner <Text> component doesn't recieve the styles we have defined in the outer <View> component */}
-      <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
-        rounds to guess the number
-        <Text style={styles.highlight}> {userNumber}</Text>.
-      </Text>
-      <View>
-        <PrimaryButton onPressProp={onStartNewGame}>
-          Start New Game
-        </PrimaryButton>
-      </View>
-    </View>
+    </ScrollView>
   );
 }
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get("window").width; // we could have also used "height" instead of "width" here. 
+// const deviceWidth = Dimensions.get("window").width; // we could have also used "height" instead of "width" here.
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+
   rootContainer: {
     flex: 1,
     padding: 24,
@@ -43,9 +74,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
-    borderRadius: deviceWidth < 380 ? 75 : 150, // width & height must be same, "borderRadius" should be half of width & height, using this we can create a circle
+    // width: deviceWidth < 380 ? 150 : 300,
+    // height: deviceWidth < 380 ? 150 : 300,
+    // borderRadius: deviceWidth < 380 ? 75 : 150, // width & height must be same, "borderRadius" should be half of width & height, using this we can create a circle
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: "hidden", // to hide the overflow nature of the actual image
