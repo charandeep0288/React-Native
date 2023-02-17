965d95ea0465e3e6dd78c174ade8829c417b4c12
charandeep0288 <ckaunsel123@gmail.com>
2023-02-17 12:56:55 +0530

    82. Adjusting Image Sizes with the Dimensions API

--- Target_App/components/game/NumberContainer.js
+++ Target_App/components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native"; // "Dimensions" is an API build into react-native
 
 import Colors from "../../constants/colors";
 
@@ -12,19 +12,21 @@ function NumberContainer({ children }) {
 
 export default NumberContainer;
 
+const deviceWidth = Dimensions.get("window").width; // we can give 2 values here -> "screen" or "window", For Android "screen" is width and height of screen + StatusBar & "window" is excluding the StatusBar,but for iOS there no difference btw screen or window, 
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: 24,
-    margin: 24,
+    padding: deviceWidth < 380 ? 12 : 24,
+    margin: deviceWidth < 380 ? 12 : 24,
     borderRadius: 8, // "borderRadius" not supported on <Text> element
     alignItems: "center",
     justifyContent: "center",
   },
   numberText: {
     color: Colors.accent500,
-    fontSize: 36,
+    fontSize: deviceWidth < 380 ? 24 : 36,
     // fontWeight: "bold",
     fontFamily: "open-sans-bold", 
   },
--- Target_App/components/ui/Card.js
+++ Target_App/components/ui/Card.js
@@ -1,15 +1,17 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
-function Card({ children }) {
+function Card({ children }) {  
   return <View style={styles.card}>{children}</View>;
 }
 
 export default Card;
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   card: {
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
