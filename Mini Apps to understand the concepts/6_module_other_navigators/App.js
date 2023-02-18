3d915c3420e31d9d4406d923c45e19f32292eb16
charandeep0288 <ckaunsel123@gmail.com>
2023-02-19 01:47:48 +0530

    108. Adding Drawer Navigation & Creating a Drawer

--- Mini Apps to understand the concepts/6_module_other_navigators/App.js
+++ Mini Apps to understand the concepts/6_module_other_navigators/App.js
@@ -1,5 +1,21 @@
-import { Text } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+ 
+const Drawer = createDrawerNavigator(); // have 2 properties(Navigator, Screen)
 
 export default function App() {
-  return <Text>Todo...</Text>;
+  // STEP 1: always to add this <NavigationContainer> if we want to use these navigators.
+  return <NavigationContainer>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Welcome" component={WelcomeScreen}></Drawer.Screen>
+      <Drawer.Screen name="User" component={UserScreen}></Drawer.Screen>
+    </Drawer.Navigator>
+  </NavigationContainer>
 }
+
+// npm install @react-navigation/drawer
+// expo install react-native-gesture-handler react-native-reanimated
+// npm install react-native-reanimated@1 --save --save-exact
