4d00f21a4001881d96ba68399a8e0e3e5694c658
charandeep0288 <ckaunsel123@gmail.com>
2023-02-20 17:55:19 +0530

    117. Using the Created Context with useContext - now can toggle Favorites icon on MealDetailScreen

--- MEALS_APP/screens/MealDetailScreen.js
+++ MEALS_APP/screens/MealDetailScreen.js
@@ -1,31 +1,44 @@
-import { useLayoutEffect } from "react";
-import { View, Text, Image, StyleSheet, ScrollView, } from "react-native";
+import { useContext, useLayoutEffect } from "react";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 import IconButton from "../components/IconButton";
 import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data"; // we can get that meal data using the meal id we get in the "route"
+import { FavoritiesContext } from "../store/context/favorites-context";
 
 function MealDetailScreen({ route, navigation }) {
+  const favoriteMealCtx = useContext(FavoritiesContext); // we need to send the "FavoritiesContext" as argument, which is the context we created in the "favorites-context.js" file
+
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  function headerButtonPressHandler() {
-    console.log("Pressed!"); 
+  const mealIsFavorite = favoriteMealCtx.ids.includes(mealId);
+
+  function ChangeFavoriteStatusHandler() {
+    // console.log("Pressed!");
+    if(mealIsFavorite) {
+      favoriteMealCtx.removeFavorite(mealId);
+    } else {
+      favoriteMealCtx.addFavorite(mealId);
+    }
   }
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <IconButton
-        icon="star"
-        color="white" 
-        onPressProp={headerButtonPressHandler}/>
-      }
-    })
-  }, [navigation, headerButtonPressHandler]);
+        return (
+          <IconButton
+            icon={mealIsFavorite ? "star" : "star-outline"}
+            color="white"
+            onPressProp={ChangeFavoriteStatusHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, ChangeFavoriteStatusHandler]);
 
   return (
     <ScrollView style={styles.rootContainer}>
