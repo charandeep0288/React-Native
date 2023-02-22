eb55bac2e8ec684fe1176e2c5efa02803b87304c
charandeep0288 <ckaunsel123@gmail.com>
2023-02-22 17:57:01 +0530

    137. Styling The Expense Management Screen

--- Expense_Tracker_App/App.js
+++ Expense_Tracker_App/App.js
@@ -68,7 +68,12 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: "white",
+          }}
+        >
           <Stack.Screen
             name="ExpensesOverview"
             component={ExpensesOverview}
@@ -77,6 +82,9 @@ export default function App() {
           <Stack.Screen
             name="ManageExpense"
             component={ManageExpense}
+            options={{
+              presentation: "modal", // to change how screen would be loaded (major impact on iOS, but looks same on Android)
+            }}
           ></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
