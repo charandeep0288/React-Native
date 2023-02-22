079fc77a3a0ba26fe289dff3ec06a1e4e4f4e19f
charandeep0288 <ckaunsel123@gmail.com>
2023-02-22 19:12:32 +0530

    139. Adding a 'Delete' Button

--- Expense_Tracker_App/screens/ManageExpense.js
+++ Expense_Tracker_App/screens/ManageExpense.js
@@ -1,12 +1,13 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/style";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId; // if we get an "id" in "editedExpenseId" variable that means are editing an expense & if we get "undefined" that means we failed to retrieve the "id" which means we are adding a new expense.
 
   const isEditing = !!editedExpenseId; // using "!!" in front of a variable we can convert a value into a boolean value, which is true if "id" exists and false if "id" doesn't exists.
-
-  console.log(route.pramas);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -14,7 +15,37 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  return <Text>ManageExpense Screen</Text>;
+  function deleteExpenseHandler() {}
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPressProp={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    margin: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
