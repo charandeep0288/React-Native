91f0cdd0e03ccb24f319ef7ad42a05231e15a913
charandeep0288 <ckaunsel123@gmail.com>
2023-02-22 14:19:37 +0530

    130. Adding Dummy Expense Data

--- Expense_Tracker_App/components/ExpensesOutput/ExpensesOutput.js
+++ Expense_Tracker_App/components/ExpensesOutput/ExpensesOutput.js
@@ -3,11 +3,45 @@ import { View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-function ExpensesOutput({ expenses, expensesPeriod }) { // "expenses" is an object 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of Shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 98.32,
+    date: new Date("2021-01-03"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.3542,
+    date: new Date("2021-12-03"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 14.32,
+    date: new Date("2021-02-19"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 18.32,
+    date: new Date("2021-02-18"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  // "expenses" is an object
   return (
     <View>
       {/* SUMMARY */}
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
 
       {/* LIST OF EXPENSES */}
       <ExpensesList />
