e81e00d07b0e09a24a5a672b8a00319822b32f7a
charandeep0288 <ckaunsel123@gmail.com>
2023-02-22 18:48:15 +0530

    138. BUG - params typo error

--- Expense_Tracker_App/screens/ManageExpense.js
+++ Expense_Tracker_App/screens/ManageExpense.js
@@ -2,9 +2,11 @@ import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
 function ManageExpense({ route, navigation }) {
-  const editedExpenseId = route.pramas?.expenseId; // if we get an "id" in "editedExpenseId" variable that means are editing an expense & if we get "undefined" that means we failed to retrieve the "id" which means we are adding a new expense.
+  const editedExpenseId = route.params?.expenseId; // if we get an "id" in "editedExpenseId" variable that means are editing an expense & if we get "undefined" that means we failed to retrieve the "id" which means we are adding a new expense.
 
   const isEditing = !!editedExpenseId; // using "!!" in front of a variable we can convert a value into a boolean value, which is true if "id" exists and false if "id" doesn't exists.
+
+  console.log(route.pramas);
 
   useLayoutEffect(() => {
     navigation.setOptions({
