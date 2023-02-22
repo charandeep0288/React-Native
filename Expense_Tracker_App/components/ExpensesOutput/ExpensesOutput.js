d3ef4fc201a44ade590e5a1c5530632ffb2d1ece
charandeep0288 <ckaunsel123@gmail.com>
2023-02-22 14:10:16 +0530

    129. Continuing Work on the Expenses-related Components

--- Expense_Tracker_App/components/ExpensesOutput/ExpensesOutput.js
+++ Expense_Tracker_App/components/ExpensesOutput/ExpensesOutput.js
@@ -3,11 +3,11 @@ import { View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-function ExpensesOutput({ expenses }) {
+function ExpensesOutput({ expenses, expensesPeriod }) { // "expenses" is an object 
   return (
     <View>
       {/* SUMMARY */}
-      <ExpensesSummary />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod}/>
 
       {/* LIST OF EXPENSES */}
       <ExpensesList />
--- Expense_Tracker_App/components/ExpensesOutput/ExpensesSummary.js
+++ Expense_Tracker_App/components/ExpensesOutput/ExpensesSummary.js
@@ -1,13 +1,18 @@
 import { View, Text } from "react-native";
 
-function ExpensesSummary() {
+function ExpensesSummary({ expenses, periodName }) {
+  // .reduce() is a standard JS method that can be executed on Arrays and it allows us to combine multiple values an array into a single value, eg: Single Array
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount; 
+  }, 0); // reduce((sum, expense) => { return returned_value_will_be_carried_over_to_the_sum_vairable }, intial_value_of_sum_variable) // "sum" is a carryover value which would be there.
+
   return (
     //   {/* SUMMARY */}
     <View>
       {/* PERIOD */}
-      <Text>Last 7 Days</Text>
+      <Text>{periodName}</Text>
       {/* SUM IN DOLLORS */}
-      <Text>$177.95</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
