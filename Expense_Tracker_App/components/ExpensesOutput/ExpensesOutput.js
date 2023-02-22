8df21c97431bf0da9a56ac1a4905ebf7b53ac91f
charandeep0288 <ckaunsel123@gmail.com>
2023-02-22 14:47:21 +0530

    132. Improving App Layout & Styling

--- Expense_Tracker_App/components/ExpensesOutput/ExpensesOutput.js
+++ Expense_Tracker_App/components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,6 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import { GlobalStyles } from "../../constants/style";
 
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
@@ -39,14 +41,22 @@ const DUMMY_EXPENSES = [
 function ExpensesOutput({ expenses, expensesPeriod }) {
   // "expenses" is an object
   return (
-    <View>
+    <View style={styles.container}>
       {/* SUMMARY */}
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
 
       {/* LIST OF EXPENSES */}
-      <ExpensesList expenses={DUMMY_EXPENSES}  />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
--- Expense_Tracker_App/components/ExpensesOutput/ExpensesSummary.js
+++ Expense_Tracker_App/components/ExpensesOutput/ExpensesSummary.js
@@ -1,20 +1,43 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/style";
 
 function ExpensesSummary({ expenses, periodName }) {
   // .reduce() is a standard JS method that can be executed on Arrays and it allows us to combine multiple values an array into a single value, eg: Single Array
   const expensesSum = expenses.reduce((sum, expense) => {
-    return sum + expense.amount; 
+    return sum + expense.amount;
   }, 0); // reduce((sum, expense) => { return returned_value_will_be_carried_over_to_the_sum_vairable }, intial_value_of_sum_variable) // "sum" is a carryover value which would be there.
 
   return (
     //   {/* SUMMARY */}
-    <View>
+    <View style={styles.container}>
       {/* PERIOD */}
-      <Text>{periodName}</Text>
+      <Text style={styles.period}>{periodName}</Text>
       {/* SUM IN DOLLORS */}
-      <Text>${expensesSum.toFixed(2)}</Text>
+      <Text style={styles.sum}>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
 
 export default ExpensesSummary;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 6,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary400,
+  },
+
+  sum: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: GlobalStyles.colors.primary500,
+  },
+});
