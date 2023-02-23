e04a1e08d7d28c867bc4def3696fb4b940a6893e
charandeep0288 <ckaunsel123@gmail.com>
2023-02-23 14:44:24 +0530

    150. Adding Styling - on Input.js file

--- Expense_Tracker_App/components/ManageExpense/Input.js
+++ Expense_Tracker_App/components/ManageExpense/Input.js
@@ -1,11 +1,45 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/style";
 
-function Input({label, textInputConfig}) { // "textInputConfig" is an object that contains all the styles that we want to apply on the <TextInput>
-    return <View>
-        {/* Label for Input field */}
-        <Text>{label}</Text>
-        <TextInput {...textInputConfig} />
+function Input({ label, textInputConfig }) {
+  // "textInputConfig" is an object that contains all the styles that we want to apply on the <TextInput>
+
+  const inputStyles = [styles.input];
+
+  if(textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMutltiLine);
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      {/* Label for Input field */}
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
+  );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary500,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMutltiLine: {
+    minHeight: 100,
+    textAlignVertical: "top", // used this to have same behaviour in both platforms
+  },
+});
