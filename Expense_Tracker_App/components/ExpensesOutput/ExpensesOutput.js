42ed55288b36ac126bfde194d334b7afd67bdd23
charandeep0288 <ckaunsel123@gmail.com>
2023-02-23 11:57:26 +0530

    145. Finishing Touches - Section 8 completed

--- Expense_Tracker_App/components/ExpensesOutput/ExpensesOutput.js
+++ Expense_Tracker_App/components/ExpensesOutput/ExpensesOutput.js
@@ -1,19 +1,24 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 import { GlobalStyles } from "../../constants/style";
 
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-function ExpensesOutput({ expenses, expensesPeriod }) {
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>; // fall back text
+
+  if (expenses.length > 0) {
+    // {/* LIST OF EXPENSES */}
+    content = <ExpensesList expenses={expenses} />;
+  }
+
   // "expenses" is an object
   return (
     <View style={styles.container}>
       {/* SUMMARY */}
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-
-      {/* LIST OF EXPENSES */}
-      <ExpensesList expenses={expenses} />
+      {content}
     </View>
   );
 }
@@ -27,5 +32,11 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
   },
 });
--- Expense_Tracker_App/screens/RecentExpenses.js
+++ Expense_Tracker_App/screens/RecentExpenses.js
@@ -16,7 +16,11 @@ function RecentExpenses() {
   // console.log(recentExpenses);
 
   return (
-    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" />
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses registered for the last 7 days"
+    />
   );
 }
 
