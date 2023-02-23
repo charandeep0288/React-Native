b6f0d272b5efe368e15aa1d1f49484568a129560
charandeep0288 <ckaunsel123@gmail.com>
2023-02-23 15:37:41 +0530

    152. Handling User Input in a Generic Way

--- Expense_Tracker_App/components/ManageExpense/ExpenseForm.js
+++ Expense_Tracker_App/components/ManageExpense/ExpenseForm.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import Input from "./Input";
 
 function ExpenseForm() {
-  function amountChangeHandler() {}
+//   const [amountValue, setAmountValue] = useState(""); // we have intial value "" because what value we get from the TextInput is a string, ever if we have entered a number, so we have used this "Empty string(.)" here instead of a numaric value.
+
+  const [inputValues, setInputValues] = useState({ // helps us to reduce the redundant code
+    amount: "",
+    date: "",
+    description: ""
+  }); // instead of creating 3 useState() for individual case here, we can also create only one useState() to handle all the States
+
+  function inputChangedHandler(inputIdentifier, enteredValue) { // react-native gives us the entered text, if we connect this fn to the "onChangeText" effect. And "inputIdentifier" we have to send this value to the fn when calling it because react-native doesn't know about this "inputIdentifier" value
+    setInputValues((currentInputValues) => {
+        return {
+            ...currentInputValues,
+            [inputIdentifier]: enteredValue // JS functionality to set and target a property dynamically
+        }
+    }); // if we update state based on previous state we use this function format
+  }
+
 
   return (
     <View style={styles.form}>
@@ -13,7 +30,8 @@ function ExpenseForm() {
           label="Amount"
           textInputConfig={{
             keyboardType: "decimal-pad",
-            onChangeText: amountChangeHandler,
+            onChangeText: inputChangedHandler.bind(this, "amount"), // "enteredValue" we don't need to sent this value because this will be provided by the react-native by default as a second parameter
+            value: inputValues["amount"],
           }}
         />
         <Input
@@ -22,7 +40,8 @@ function ExpenseForm() {
           textInputConfig={{
             placeholder: "YYYY-MM-DD",
             maxLength: 10,
-            onChangeText: () => {},
+            onChangeText: inputChangedHandler.bind(this, "date"), // "enteredValue" we don't need to sent this value because this will be provided by the react-native by default as a second parameter
+            value: inputValues["date"],
           }}
         />
       </View>
@@ -33,6 +52,8 @@ function ExpenseForm() {
           multiline: true,
           // autoCorrect: false, // default is true, eg:- we don't want this "autoCorrect" when we are writting an email, , that would create lots of problems.
           // autoCapitalize: "characters", // "sentences" by default are Capitalized, other options -> "characters", "words", "none"......... eg:- we don't want this "autoCapitalize" when we are writting an email, that would create lots of problems.
+          onChangeText: inputChangedHandler.bind(this, "description"), // "enteredValue" we don't need to sent this value because this will be provided by the react-native by default as a second parameter
+          value: inputValues["description"],
         }}
       />
     </View>
