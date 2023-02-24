1326483395020725115a1b2722ded4f22201ea4e
charandeep0288 <ckaunsel123@gmail.com>
2023-02-24 13:17:35 +0530

    Resolved BUG in - 163. Sending POST Http Requests

--- Expense_Tracker_App/util/http.js
+++ Expense_Tracker_App/util/http.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export function storeExpense(expenseData) {
   // axios.post("URL_as_string_to_which_request_should_be_sent_first_arguement", value_which_we_want_to_send)
   axios.post(
-    "https://console.firebase.google.com/u/0/project/expense-tracker-app-2c286/database/expense-tracker-app-2c286-default-rtdb/data/~2F/expenses.json", // firebase want this .json at the last that means we are targing node in the backend
+    "https://expense-tracker-app-2c286-default-rtdb.firebaseio.com/expenses.json", // firebase want this .json at the last that means we are targing node in the backend
     expenseData
   ); // to sending post request to send data
 }
