937a6a7a80737a6f7ca06ba9b7e9fcfe66decb51
charandeep0288 <ckaunsel123@gmail.com>
2023-02-24 15:00:19 +0530

    164. Fetching Backend Data(GET Requests)

--- Expense_Tracker_App/screens/RecentExpenses.js
+++ Expense_Tracker_App/screens/RecentExpenses.js
@@ -1,10 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expneses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
   const expenseCtx = useContext(ExpensesContext);
+
+  // HTTTP Request is an asyncronous task
+  useEffect(() => { // we can't convert useEffect's fn in argument into async await, so we have to do a work around by creating another function in useEffect fn and calling it in this useEffect 
+    async function getExpense() {
+      const expenses = await fetchExpenses();
+    }
+
+    getExpense();
+  }, []);
 
   const recentExpenses = expenseCtx.expenses.filter((expense) => {
     const today = new Date();
