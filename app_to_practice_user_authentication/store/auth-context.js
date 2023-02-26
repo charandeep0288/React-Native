febeb009d0f40b042cbaf12f68f6eefe8327b743
charandeep0288 <ckaunsel123@gmail.com>
2023-02-26 17:13:25 +0530

    180. Storing & Managing the User Authentication State(with Context)

---
+++ app_to_practice_user_authentication/store/auth-context.js
@@ -0,0 +1,31 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext({
+  token: "",
+  isAuthenticated: false,
+  authenticate: (token) => {}, // to check if a user SignedIn or LoggedIn successfully.
+  logout: () => {}, // this would create the authentication status and token
+});
+
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState(); // Initial value of the auth token in undefined
+
+  function authenticate(token) {
+    setAuthToken(token);
+  }
+
+  function logout() {
+    setAuthToken(null);
+  }
+
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
+
+  return <AuthContextProvider value={value}>{children}</AuthContextProvider>;
+}
+
+export default AuthContextProvider;
