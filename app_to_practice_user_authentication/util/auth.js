078bfc965ead0aa232c846f5c57e891fbdabf3e5
charandeep0288 <ckaunsel123@gmail.com>
2023-02-26 16:18:12 +0530

    178. Logging Users in

--- app_to_practice_user_authentication/util/auth.js
+++ app_to_practice_user_authentication/util/auth.js
@@ -2,14 +2,23 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyBQLDO2i3Fbnrih6W28f-JbFIH_yo5Zw28";
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+}
+
 export async function createUser(email, password) {
   // wraped this function into async await because we want to show a loading screen while this action is being performed in the backend
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+    await authenticate("signInWithPassword", email, password);
 }
