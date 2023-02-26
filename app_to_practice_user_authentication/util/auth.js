897705fcfb8abda9a3c92c7076e3cb00bdc73180
charandeep0288 <ckaunsel123@gmail.com>
2023-02-26 15:58:28 +0530

    177. Creating New Users

--- app_to_practice_user_authentication/util/auth.js
+++ app_to_practice_user_authentication/util/auth.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyBQLDO2i3Fbnrih6W28f-JbFIH_yo5Zw28";
 
-async function createUser(email, passord) { // wraped this function into async await because we want to show a loading screen while this action is being performed in the backend
+export async function createUser(email, password) {
+  // wraped this function into async await because we want to show a loading screen while this action is being performed in the backend
   const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=" +
-      API_KEY,
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
       email: email,
       password: password,
