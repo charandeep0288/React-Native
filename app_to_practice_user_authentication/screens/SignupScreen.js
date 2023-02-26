442b1086b5a422dbf1e97ec84763ce1b8648212d
charandeep0288 <ckaunsel123@gmail.com>
2023-02-26 16:34:02 +0530

    179. Authentication Error Handling

--- app_to_practice_user_authentication/screens/SignupScreen.js
+++ app_to_practice_user_authentication/screens/SignupScreen.js
@@ -1,20 +1,28 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { createUser } from "../util/auth";
 
 function SignupScreen() {
-  const [isAuthenticating, setIsAuthenticating ] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user, please check your input and try again later."
+      );
+    }
     setIsAuthenticating(false);
   }
 
-  if(isAuthenticating) {
-    return <LoadingOverlay message="Creating user..."/>
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
   }
 
   return <AuthContent onAuthenticate={signupHandler} />;
