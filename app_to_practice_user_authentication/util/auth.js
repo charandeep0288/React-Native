2ce2a43eb60b616429a330fb16f487cbef4df5ed
charandeep0288 <ckaunsel123@gmail.com>
2023-02-26 17:29:44 +0530

    181. Extracting the Authentication Token

--- app_to_practice_user_authentication/util/auth.js
+++ app_to_practice_user_authentication/util/auth.js
@@ -11,14 +11,16 @@ async function authenticate(mode, email, password) {
     returnSecureToken: true,
   });
 
-  console.log(response.data);
+  //   console.log(response.data);
+  const token = response.data.idToken; // we know the we would get token in "idToken" because we console data upthere and saw the results, or we can see on the official docs
+
+  return token;
 }
 
-export async function createUser(email, password) {
-  // wraped this function into async await because we want to show a loading screen while this action is being performed in the backend
-  await authenticate("signUp", email, password);
+export function createUser(email, password) { // wraped this function into async await because we want to show a loading screen while this action is being performed in the backend
+  return authenticate("signUp", email, password); // we can just do return and return the promise that we get & also then we can remove the async await from this fn to make things simpler.
 }
 
-export async function login(email, password) {
-    await authenticate("signInWithPassword", email, password);
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
 }
