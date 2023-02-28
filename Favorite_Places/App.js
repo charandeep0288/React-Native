38672ac0826c9873017853fd6b97c85026392471
charandeep0288 <ckaunsel123@gmail.com>
2023-02-28 23:18:45 +0530

    192. Adding a Header Button

--- Favorite_Places/App.js
+++ Favorite_Places/App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,21 @@ export default function App() {
       <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              // we can use "navigation prop" or "useNavigation" to navigate to different screen, but also here we can convert this nested object's inner object into fn and we get "navigation" as a prop in this function.
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPressProp={() => navigation.navigate("AddPlace")} // this would navigate us to the "AddPlace" Screen
+                />
+              ),
+            })}
+          />
           <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ Favorite_Places/components/UI/IconButton.js
@@ -0,0 +1,27 @@
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+function IconButton({ icon, size, color, onPressProp }) {
+  return (
+    <Pressable
+      style={[styles.button, ({ pressed }) => pressed && styles.pressed]}
+      onPress={onPressProp}
+    >
+      <Ionicons name={icon} size={size} color={color} />
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 4,
+    justifyContent: "center",
+    alignItem: "center",
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
